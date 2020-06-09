158276bb96aad30fc0cd2fd0cafe64b1e064aff6
kanokenji <aleksandrtarasevich77@gmail.com>
2020-06-09 16:23:21 -0600

    Updates

--- precache-manifest.ac695a14320b38086b91d92b1cdf4757.js
+++ precache-manifest.ac695a14320b38086b91d92b1cdf4757.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "64890dd9985a7e0d2ced1eed341eb089",
+    "revision": "a73d009ed131c1549e6c217e97b7c992",
     "url": "/kanban-lesson/index.html"
   },
   {
-    "revision": "2064ec7ffc9f452c1617",
+    "revision": "1f0e124e256b50b02217",
     "url": "/kanban-lesson/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "2064ec7ffc9f452c1617",
-    "url": "/kanban-lesson/static/js/2.999fd71d.chunk.js"
+    "revision": "1f0e124e256b50b02217",
+    "url": "/kanban-lesson/static/js/2.a4bca580.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/kanban-lesson/static/js/2.999fd71d.chunk.js.LICENSE.txt"
+    "url": "/kanban-lesson/static/js/2.a4bca580.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6d598cff36a761cc3791",
-    "url": "/kanban-lesson/static/js/main.c089609b.chunk.js"
+    "revision": "967abb86ae9e880f0687",
+    "url": "/kanban-lesson/static/js/main.703a2f5f.chunk.js"
   },
   {
     "revision": "3ab49b4beebf01112566",
